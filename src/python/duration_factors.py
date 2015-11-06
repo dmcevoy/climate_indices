97d9193bd3475f90454c2facf170472ecbc12fc4
James Adams <james.adams@noaa.gov>
2015-11-06 17:33:00 -0500

    clean up, additional comments

--- src/python/duration_factors.py
+++ src/python/duration_factors.py
@@ -1,16 +1,25 @@
 from stack import Stack
 import math
+import numpy as np
+import logging
+import numba
+
+# set up a global logger
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 MISSING_VALUE = -999.9
 
-def least_squares(x,  # array of integers 
-                  y,  # array of doubles 
-                  n,  # int
-                  wet_or_dry):  # boolean 
+#-----------------------------------------------------------------------------------------------------------------------
+@numba.jit
+def least_squares(x,  # array of integers
+                  y,  # array of doubles
+                  n,  # int, length of array x
+                  wet_or_dry):  # boolean
 
     correlation = 0.0
     c_tol = 0.85
-    max = 0.0
+    max_value = 0.0
     max_diff = 0.0
     max_i = 0
     sumX = 0.0
@@ -20,7 +29,11 @@ def least_squares(x,  # array of integers
     sumXY = 0.0
     for i in range(n):
 
-        this_x = double(x[i])
+        # make sure we don't use NaN values
+        if np.isnan(y[i]):
+            continue
+
+        this_x = float(x[i])
         this_y = y[i]
         sumX += this_x
         sumY += this_y
@@ -31,20 +44,23 @@ def least_squares(x,  # array of integers
     SSX = sumX2 - (sumX * sumX) / n
     SSY = sumY2 - (sumY * sumY) / n
     SSXY = sumXY - (sumX * sumY) / n
-    correlation = SSXY / (math.sqrt(SSX) * math.sqrt(SSY))
+    if (SSX > 0.0) and (SSY > 0.0):
+        correlation = SSXY / (math.sqrt(SSX) * math.sqrt(SSY))
+    else:
+        logger.warn('SSX: {ssx}  SSY: {ssy}'.format(ssx=SSX, ssy=SSY))
     i = n - 1
-    
-    # if we're dealing with wet conditions then we want to be using positive numbers, and for dry conditions  
-    # then we want to be using negative numbers, so we introduce a sign variable to facilitate this 
+
+    # if we're dealing with wet conditions then we want to be using positive numbers, and for dry conditions
+    # then we want to be using negative numbers, so we introduce a sign variable to facilitate this
     if wet_or_dry == 'dry':
         sign = -1
     else:
         sign = 1
-    
+
     while ((sign * correlation) < c_tol) and (i > 3):
         # when the correlation is off, it appears better to
         # take the earlier sums rather than the later ones.
-        this_x = double(x[i])
+        this_x = float(x[i])
         this_y = y[i]
         sumX -= this_x
         sumY -= this_y
@@ -54,26 +70,30 @@ def least_squares(x,  # array of integers
         SSX = sumX2 - (sumX * sumX) / i
         SSY = sumY2 - (sumY * sumY) / i
         SSXY = sumXY - (sumX * sumY) / i
-        correlation = SSXY / (math.sqrt(SSX) * math.sqrt(SSY))
+        if (SSX > 0.0) and (SSY > 0.0):
+            correlation = SSXY / (math.sqrt(SSX) * math.sqrt(SSY))
+        else:
+            logger.warn('SSX: {ssx}  SSY: {ssy}'.format(ssx=SSX, ssy=SSY))
         i -= 1
-    
+
     least_squares_slope = SSXY / SSX
     for j in range(i + 1):
-        
+
         if sign * (y[j] - (least_squares_slope * x[j])) > (sign * max_diff):
-        
+
             max_diff = y[j] - least_squares_slope * x[j];
             max_i = j;
-            max = y[j];
-         
-    leastSquaresIntercept = max - least_squares_slope * x[max_i]
-    
-    return least_squares_slope, leastSquaresIntercept
-    
+            max_value = y[j];
+
+    least_squares_intercept = max_value - least_squares_slope * x[max_i]
+
+    return least_squares_slope, least_squares_intercept
+
+#-----------------------------------------------------------------------------------------------------------------------
 '''
     /*
      * TODO document with reference to the relevant sections of the Palmer and/or Wells papers
-     * 
+     *
      */
     //calculates m and b, which are used to calculated X(i)
     //based on the Z index.  These constants will determine the
@@ -89,48 +109,84 @@ def least_squares(x,  # array of integers
     //negative and positive Z values, so the variable 'sign' will
     //determine whether the driest or wettest periods are looked at.
 '''
+#@numba.jit
 def compute_duration_factors(zindex_values,
                              calibration_start_year,
                              calibration_end_year,
                              input_start_year,
-                             wet_or_dry='wet',  # 'wet' or 'dry'
                              periods_per_year=12,
                              month_scale=1):
 
-    z_sum = get_z_sum(month_scale,
-                      wet_or_dry, 
-                      zindex_values, 
-                      periods_per_year,
-                      calibration_start_year, 
-                      calibration_end_year, 
-                      input_start_year)
-    
-    slope, intercept = leastSquares(month_scale, z_sum, 1, wet_or_dry)
-    
-    # if we're dealing with wet conditions then we want to be using positive numbers, and if dry conditions then
-    # we need to be using negative numbers, so we use a PDSI limit of 4 on the wet side and -4 on the dry side 
-    pdsiLimit = 4.0  # WET
-    if (wet_or_dry == 'dry'):
-        pdsiLimit = -4.0
-    
-    # now divide slope and intercept by 4 or -4 because that line represents a PDSI of either 4.0 or -4.0
-    slope = slope / pdsiLimit
-    intercept = intercept / pdsiLimit            
-    
-    if (wet_or_dry == 'wet'):
-        computationFrame.wetM = slope
-        computationFrame.wetB = intercept
-    else:  # DRY
-        computationFrame.dryM = slope
-        computationFrame.dryB = intercept
-        
+    '''
+    :param zindex_values: array with dimensions (lons, lats, times), times are months, in full year increments 
+                          (i.e. len(times) should be evenly divisible by 12)
+    :param calibration_start_year: 
+    :param calibration_end_year:
+    :param input_start_year:
+    :param periods_per_year: the number of periods we want to compute, 12 for monthly, 4 for seasonal, etc.
+    :param month_scale:   
+    '''
+    month_scales = [3, 6, 9, 12, 18, 24, 30, 36, 42, 48]
+
+    duration_factors = np.full((zindex_values.shape[0], zindex_values.shape[1], 2, 2), -999.9)
+    for lon in range(zindex_values.shape[0]):
+        for lat in range(zindex_values.shape[1]):
+            for wet_or_dry in ['wet', 'dry']:
+
+                logger.info('Computing {wet_or_dry} factors for lon/lat: {lon}/{lat}'.format(wet_or_dry=wet_or_dry, lon=lon, lat=lat))
+
+                z_sums = np.zeros((len(month_scales), ))
+                for i, month_scale in enumerate(month_scales):
+
+                    try:
+                        z_sums[i] = get_z_sum(month_scale,
+                                              wet_or_dry,
+                                              zindex_values[lon][lat],
+                                              periods_per_year,
+                                              calibration_start_year,
+                                              calibration_end_year,
+                                              input_start_year)
+                    except Exception:
+                        logger.warn('Failed to get the {wet_or_dry} Z sums for lon/lat: {lon}/{lat}'.format(wet_or_dry=wet_or_dry, lon=lon, lat=lat))
+                        raise
+
+                # get the slope and intercept for the duration factor for this lon/lat using least squares regression
+                try:
+                    slope, intercept = least_squares(month_scales, z_sums, len(month_scales), wet_or_dry)
+                except Exception:
+                    logger.warn('Failed to get the least squares {wet_or_dry} slope and intercept for lon/lat: {lon}/{lat}'.format(wet_or_dry=wet_or_dry, lon=lon, lat=lat))
+                    raise
+
+                # if we're dealing with wet conditions then we want to be using positive numbers, and if dry conditions then
+                # we need to be using negative numbers, so we use a PDSI limit of 4 on the wet side and -4 on the dry side
+                pdsiLimit = 4.0  # WET
+                wet_or_dry_index = 0
+                if (wet_or_dry == 'dry'):
+                    pdsiLimit = -4.0
+                    wet_or_dry_index = 1
+
+                # now divide slope and intercept by 4 or -4 because that line represents a PDSI of either 4.0 or -4.0
+                slope = slope / pdsiLimit
+                intercept = intercept / pdsiLimit
+
+                duration_factors[lon, lat, wet_or_dry_index, 0] = slope
+                duration_factors[lon, lat, wet_or_dry_index, 1] = intercept
+
+    # we now have a duration factors array with a 2 x 2 array for each lon/lat point, where
+    #     duration_factors[lon][lat][0][0] = wet slope for lon/lat
+    #     duration_factors[lon][lat][0][1] = wet intercept for lon/lat
+    #     duration_factors[lon][lat][1][0] = dry slope for lon/lat
+    #     duration_factors[lon][lat][1][1] = dry intercept for lon/lat
+    return duration_factors
+
 #-----------------------------------------------------------------------------------------------------------------------
-def get_z_sum(month_scale,
-              wet_or_dry, 
-              zindex_values, 
+@numba.jit
+def get_z_sum(interval_length,
+              wet_or_dry,
+              zindex_values,
               periods_per_year,
-              calibration_start_year, 
-              calibration_end_year, 
+              calibration_start_year,
+              calibration_end_year,
               input_start_year):
 
         tempZ = Stack()
@@ -142,7 +198,7 @@ def get_z_sum(month_scale,
             # we need to skip Z-index values from the list if they don't exist, this can result from empty months in the final year of the data set
             if not np.isnan(zindex):
                 tempZ.push(zindex)
-                
+
         # remove periods before the start of the calibration interval
         initial_calibration_period_index = (calibration_start_year - input_start_year) * periods_per_year
         for i in range(initial_calibration_period_index):
@@ -154,72 +210,72 @@ def get_z_sum(month_scale,
 
         calibration_periods_left = (calibration_end_year - calibration_start_year + 1) * periods_per_year;
 
-        # get the first interval length of values from the end of the calibration period working backwards, creating the first sum of interval periods
-        sum = 0
+        # get the first interval length of values from the end of the calibration period working backwards, creating the first sum_value of interval periods
+        sum_value = 0
         for i in range(interval_length):
             if tempZ.isEmpty():
                 i = interval_length
             else:
                 # pull a value off the end of the list
                 z = tempZ.pop()
-                
+
                 # reduce the remaining number of calibration interval periods we have left to process
-                calibrationPeriodsLeft -= 1
-                
+                calibration_periods_left -= 1
+
                 '''
-                /* assumes that nCalibrationPeriods is >= length, reasonable 
+                /* assumes that nCalibrationPeriods is >= length, reasonable
                  ** This is a reasonable assumption and does not hurt if
                  ** anything if false--just calibrate over a slightly longer
                  ** interval, which is already way too short in that case */
                 '''
-                if (not np.isNaN(z) and (MISSING_VALUE != z)):
+                if (not np.isnan(z) and (MISSING_VALUE != z)):
 
-                    # add to the sum
-                    sum += z
-                    
-                    # add to the array of values we've used for the initial sum
-                    values_to_sum.addFirst(z)
-                
+                    # add to the sum_value
+                    sum_value += z
+
+                    # add to the array of values we've used for the initial sum_value
+                    values_to_sum.add_first(z)
+
                 else:
                     # reduce the loop counter so we don't skip a calibration interval period
                     i -= 1
-        
-        # if we're dealing with wet conditions then we want to be using positive numbers, and if dry conditions  
-        # then we need to be using negative numbers, so we introduce a sign variable to help with this 
+
+        # if we're dealing with wet conditions then we want to be using positive numbers, and if dry conditions
+        # then we need to be using negative numbers, so we introduce a sign variable to help with this
         if wet_or_dry == 'wet':
             sign = 1
         else:  # dry
             sign =-1
-        
-        # now for each remaining Z value, recalculate the sum based on last value in the list to sum and the next Z value
-        maxSum = sum
-        summed_values.addFirst(sum)
-        while (tempZ.size() > 0) and (calibrationPeriodsLeft > 0):
 
-            # take the next Z-index value off the end of the list 
+        # now for each remaining Z value, recalculate the sum_value based on last value in the list to sum_value and the next Z value
+        max_sum = sum_value
+        summed_values.add_first(sum_value)
+        while (tempZ.size > 0) and (calibration_periods_left > 0):
+
+            # take the next Z-index value off the end of the list
             z = tempZ.pop()
 
             # reduce by one period for each removal
-            calibrationPeriodsLeft -= 1
-        
-            if not np.isnan(z) and (MISSING_VALUE != z):
+            calibration_periods_left -= 1
 
-                # come up with a new sum for this new group of values to sum
-                
-                # remove the last value from both the sum and the values to sum array
-                sum -= values_to_sum.getLast()
+            if not np.isnan(z) and (MISSING_VALUE != z) and not values_to_sum.isEmpty():
+
+                # come up with a new sum_value for this new group of values to sum_value
+
+                # remove the last value from both the sum_value and the values to sum_value array
+                sum_value -= values_to_sum.peek()
                 values_to_sum.pop()
-                
-                # add to the sum
-                sum += z
-                
-                # update the values to sum and summed values lists
-                values_to_sum.addFirst(z);
-                summed_values.addFirst(sum);
-             
-            # update the maximum sum value if we have a new max
-            if (sign * sum) > (sign * maxSum):
-                maxSum = sum
+
+                # add to the sum_value
+                sum_value += z
+
+                # update the values to sum_value and summed values lists
+                values_to_sum.add_first(z);
+                summed_values.add_first(sum_value);
+
+            # update the maximum sum_value value if we have a new max
+            if (sign * sum_value) > (sign * max_sum):
+                max_sum = sum_value
 
         # highest reasonable is the highest (or lowest) value that is not due to some freak anomaly in the data.
         # "freak anomaly" is defined as a value that is either
@@ -229,22 +285,22 @@ def get_z_sum(month_scale,
             safe_percentile_index = int(summed_values.size * 0.98)
         else:  # DRY
             safe_percentile_index = int(summed_values.size * 0.02)
-        
-        # sort the list of sums into ascending order and get the sum value referenced by the safe percentile index
-        summed_values_sorted = summed_values.list().sort()
-        sum_at_safe_percentile = summed_values_sorted[safe_percentile_index]
-          
+
+        # sort the list of sums into ascending order and get the sum_value value referenced by the safe percentile index
+        summed_values_list = summed_values.sorted_list()
+        sum_at_safe_percentile = summed_values_list[safe_percentile_index]
+
         # find the highest reasonable value out of the summed values
         highest_reasonable_value = 0.0
         reasonable_tolerance_ratio = 1.25
         while (summed_values.size > 0):
-            sum = summed_values.pop()
-            if (sign * sum > 0):
-                if ((sum / sum_at_safe_percentile) < reasonable_tolerance_ratio):
-                    if (sign * sum > sign * highest_reasonable_value):
-                        highest_reasonable_value = sum
-        
+            sum_value = summed_values.pop()
+            if (sign * sum_value > 0):
+                if ((sum_value / sum_at_safe_percentile) < reasonable_tolerance_ratio):
+                    if (sign * sum_value > sign * highest_reasonable_value):
+                        highest_reasonable_value = sum_value
+
         if wet_or_dry == 'wet':
             return highest_reasonable_value
         else:  # DRY
-            return maxSum
+            return max_sum
