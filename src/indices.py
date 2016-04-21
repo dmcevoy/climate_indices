82014444c6331cfbda0153d561f2dec6739655db
James Adams <james.adams@noaa.gov>
2016-04-21 12:24:27 -0400

    removed the input data check from the spi_gamma() function

--- src/indices.py
+++ src/indices.py
@@ -35,17 +35,11 @@ def spi_gamma(precip_monthly_values,
               valid_max):
 
     
-    if isinstance(precip_monthly_values, np.ma.MaskedArray) and precip_monthly_values.mask.all():
-        
-        return distribution_fitter.fit_to_gamma(precip_monthly_values, 
+    return distribution_fitter.fit_to_gamma(precip_monthly_values, 
                                             month_scale, 
                                             valid_min, 
                                             valid_max)
-    else:
-        
-        # we have only NaNs in the input dataset, return a NaN array in response (don't attempt to compute)
-        return np.full(precip_monthly_values.shape(), np.NaN)
-    
+
 #-----------------------------------------------------------------------------------------------------------------------
 def spi_pearson(precip_monthly_values,
                 month_scale, 
